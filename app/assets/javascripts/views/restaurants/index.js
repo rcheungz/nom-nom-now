614c21a0f7f90ca20cf062121c3a3468767d4aee
Zach Chen <zachen717@gmail.com>
2014-09-26 14:23:43 -0700

    added functionality to use current position

--- app/assets/javascripts/views/restaurants/index.js
+++ app/assets/javascripts/views/restaurants/index.js
@@ -15,11 +15,12 @@ NomNom.Views.RestaurantIndex = Backbone.View.extend({
 	render: function () { //doesn't retain data after refresh
 		var renderedContent = this.template({
 			restaurants: this.collection,
-			function: this.codeAddress
+			// function: this.codeAddress
 			//restaurants: this.filterCollection
 		});
 		this.$el.html(renderedContent);
-		this.initializeMap();//important this must stay here so that after the page renders then the map pops up so it isn't removed.
+		// this.initializeMap();//important this must stay here so that after the page renders then the map pops up so it isn't removed.
+		this.currentLocation();
 		return this;
 	},
 	
@@ -32,6 +33,52 @@ NomNom.Views.RestaurantIndex = Backbone.View.extend({
 		console.log("entered initialize map first");
 	  this.map = new google.maps.Map(this.$('#map-canvas')[0],
 	      mapOptions);
+	},
+	
+	currentLocation: function () {
+	  var mapOptions = {
+	    zoom: 6
+	  };
+	  this.map = new google.maps.Map(this.$('#map-canvas')[0],
+	      mapOptions);
+
+	  // Try HTML5 geolocation
+		var that = this;
+	  if(navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	      var pos = new google.maps.LatLng(position.coords.latitude,
+	                                       position.coords.longitude);
+
+	      var infowindow = new google.maps.InfoWindow({
+	        map: that.map,
+	        position: pos,
+	        content: 'Location found using HTML5.'
+	      });
+	      that.map.setCenter(pos);
+	    }, function() {
+	      handleNoGeolocation(true);
+	    });
+	  } else {
+	    // Browser doesn't support Geolocation
+	    handleNoGeolocation(false);
+	  }
+	},
+	
+	handleNoGeolocation: function (errorFlag) {
+	  if (errorFlag) {
+	    var content = 'Error: The Geolocation service failed.';
+	  } else {
+	    var content = 'Error: Your browser doesn\'t support geolocation.';
+	  }
+
+	  var options = {
+	    map: this.map,
+	    position: new google.maps.LatLng(60, 105),
+	    content: content
+	  };
+
+	  var infowindow = new google.maps.InfoWindow(options);
+	  this.map.setCenter(options.position);
 	},
 	
 	codeAddress: function () {
