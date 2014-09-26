9d84943fdfafaacc02899f31ba40005607162f53
Zach Chen <zachen717@gmail.com>
2014-09-26 14:13:48 -0700

    got geocoder to work

--- app/assets/javascripts/views/restaurants/index.js
+++ app/assets/javascripts/views/restaurants/index.js
@@ -3,6 +3,10 @@ NomNom.Views.RestaurantIndex = Backbone.View.extend({
 	
 	className: "restaurants-index",
 	
+	events: {
+		"click .geocode": "codeAddress"
+	},
+	
 	initialize: function (options) {
 		this.searchString = options.keyword;
 		this.listenTo(this.collection, "sync", this.render);
@@ -10,22 +14,45 @@ NomNom.Views.RestaurantIndex = Backbone.View.extend({
 	
 	render: function () { //doesn't retain data after refresh
 		var renderedContent = this.template({
-			restaurants: this.collection
+			restaurants: this.collection,
+			function: this.codeAddress
 			//restaurants: this.filterCollection
 		});
 		this.$el.html(renderedContent);
-		this.initializeMap();
+		this.initializeMap();//important this must stay here so that after the page renders then the map pops up so it isn't removed.
 		return this;
 	},
 	
 	initializeMap: function () {
+		this.geocoder = new google.maps.Geocoder();
 		var mapOptions = {
       center: { lat: 37.751994, lng: -122.443341},
-      zoom: 13
+      zoom: 12
     };
-
-	  var map = new google.maps.Map(this.$('#map-canvas')[0],
+		console.log("entered initialize map first");
+	  this.map = new google.maps.Map(this.$('#map-canvas')[0],
 	      mapOptions);
+	},
+	
+	codeAddress: function () {
+	  var address = document.getElementById('address').value;
+		debugger;
+		var that = this;
+	  this.geocoder.geocode( { 'address': address}, function(results, status) {
+			console.log("entered geocoder loop");
+			debugger;
+	    if (status == google.maps.GeocoderStatus.OK) {
+				console.log("entered status loop");
+				debugger;
+	      that.map.setCenter(results[0].geometry.location);
+	      var marker = new google.maps.Marker({
+	          map: that.map,
+	          position: results[0].geometry.location
+	      });
+	    } else {
+				alert('Geocode was not successful for the following reason: ' + status);
+	    }
+	  });
 	},
 	
 	// onRender: function () {
